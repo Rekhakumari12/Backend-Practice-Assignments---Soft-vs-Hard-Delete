c8320be9b782f6feb8307f5546cd42241995152b
Rekha <60341606+Rekhakumari12@users.noreply.github.com>
2021-06-21 11:46:29 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -12,16 +12,32 @@ app.use(express.json());
 // Get all the students
 app.get('/students', async (req, res) => {
     // write your codes here
+    const students = await Student.find()
+    if (!students) return statusCode(404)
+    res.send(students)
 })
 
 // Add student to database
 app.post('/students', async (req, res) =>{
     // write your codes here
+     const student = new Student({
+        name: req.body.name,
+        sex: req.body.sex,
+        class: req.body.class,
+        age: req.body.age,
+        grade_point: req.body.grade_point,
+       isDeleted:req.body.isDeleted
+    })
+    const result = await student.save()
+    res.send(result)
 })
 
 // Get specific student
 app.get('/students/:id', async (req, res) =>{
     // write your codes here
+     const student=await Student.findById(req.params.id)
+    if (!student) return statusCode(404)
+    res.send(student)
 })
 
 // delete specific student
